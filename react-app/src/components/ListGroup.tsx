0dad112cdb2cba63fcf70a3538448b300ea54d38
kkavinkk <gk.kavin05@gmail.com>
2025-02-10 10:40:54 -0700

    frags are weird but weird is gut

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,20 +1,23 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  const items = [
-    "New York",
-    "San Francisco",
-    "San Francisco",
-    "Tokyo",
-    "Paris",
-  ];
+  let items = ["New York", "San Francisco", "San Francisco", "Tokyo", "Paris"];
+  items = [];
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li key={item}>{item}</li>
         ))}
         ;
       </ul>
