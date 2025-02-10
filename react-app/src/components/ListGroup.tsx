747cfc595921156ec3a8e367e175e352b91fa870
kkavinkk <gk.kavin05@gmail.com>
2025-02-10 10:32:58 -0700

    MUSTARDDDDDD

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,8 +1,16 @@
 import { Fragment } from "react";
 
 function ListGroup() {
+  const items = [
+    "New York",
+    "San Francisco",
+    "San Francisco",
+    "Tokyo",
+    "Paris",
+  ];
+
   return (
-    <Fragment>
+    <>
       <h1>List</h1>
       <ul className="list-group">
         <li className="list-group-item">An item</li>
@@ -11,7 +19,7 @@ function ListGroup() {
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li>
       </ul>
-    </Fragment>
+    </>
   );
 }
 
