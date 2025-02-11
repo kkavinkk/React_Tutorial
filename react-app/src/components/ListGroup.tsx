5e78bab0e3811b2ca43739e1842c61d3922f0ac3
kkavinkk <gk.kavin05@gmail.com>
2025-02-11 14:41:10 -0700

    moving forward

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,17 +1,21 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  let items = ["New York", "San Francisco", "San Francisco", "Tokyo", "Paris"];
-  items = [];
+  let items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
 
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? <p>No items Found</p> : null}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
