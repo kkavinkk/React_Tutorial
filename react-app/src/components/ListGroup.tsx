ab290405a7c4ecb6b334b232ebc5b6e97f51458c
kkavinkk <gk.kavin05@gmail.com>
2025-02-11 14:30:22 -0700

    moving forward

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -4,19 +4,15 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "San Francisco", "Tokyo", "Paris"];
   items = [];
 
-  const getMessage = () => {
-    items.length === 0 ? <p>No items Found</p> : null;
-  };
-
   return (
     <>
       <h1>List</h1>
-      {getMessage(1)}
+      {items.length === 0 ? <p>No items Found</p> : null}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
         ))}
-        ;
       </ul>
     </>
   );
