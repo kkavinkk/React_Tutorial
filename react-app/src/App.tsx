dc20896168ec646e4612807647418ae199e2f083
kkavinkk <gk.kavin05@gmail.com>
2025-02-14 23:12:24 -0700

    Pushing forard

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,10 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
