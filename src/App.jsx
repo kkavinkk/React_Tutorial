94dae09ee25de01d4706ab49cd7c2b3f511b4f2b
Kavin Kumaran <gk.kavin05@gmail.com>
2025-02-19 11:38:55 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+        </li>
+      </ul>
+    </>
+  )
+}
